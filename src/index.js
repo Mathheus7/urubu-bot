a133bd8cfd52a976b87301f738031d57533bbc6a
Matheus Franca <matheusgustavo0008@yahoo.com>
2023-03-11 14:44:34 -0300

    iniciando o urubu

---
+++ src/index.js
@@ -0,0 +1,40 @@
+const { REST, Routes } = require('discord.js');
+const { Client, GatewayIntentBits } = require('discord.js');
+require('dotenv').config()
+
+const commands = [
+  {
+    name: 'ping',
+    description: 'Replies with Pong!',
+  },
+];
+
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands });
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
